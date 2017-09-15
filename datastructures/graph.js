7752f419189fb8dcff9df7d1f0d33609212206a7
Sharad Jain <shjain@expedia.com>
2017-09-15 09:47:55 +0530

    Adding depth first search

--- datastructures/graph.js
+++ datastructures/graph.js
@@ -13,3 +13,30 @@ Graph.prototype.addEdge = function (v1, v2) {
     this._node[v1].push(v2);
     this._node[v2].push(v1);
 }
+
+Graph.prototype.depthFirstSearch = function (value, fn, visited = {}, distance = 0) {
+    if (!this._node[value] || typeof fn !== 'function') {
+        return "invalid value or function";
+    }
+    fn(value,distance);
+    visited[value] = true;
+    this._node[value].forEach(function(neighbour){
+        if(visited[neighbour]) return;
+        this.depthFirstSearch(neighbour,fn,visited,distance+1);
+    },this)
+}
+
+var myGraph = new Graph();
+myGraph.addNode(1);
+myGraph.addNode(2);
+myGraph.addNode(3);
+myGraph.addNode(4);
+myGraph.addNode(5);
+myGraph.addNode(6);
+myGraph.addEdge(1,2);
+myGraph.addEdge(1,3);
+myGraph.addEdge(2,4);
+myGraph.addEdge(4,5);
+myGraph.addEdge(5,6);
+
+myGraph.depthFirstSearch(1,(value,distance) => console.log(value,distance));
